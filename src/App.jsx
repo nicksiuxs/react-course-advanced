99e5fbd832e5e01bc680d2910ea2ac3469e1d007
Nicolas Londono <nicksiuxs@gmail.com>
2025-05-05 19:28:00 -0500

    Add: first container component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css'
+import UserApp from "./design-patterns/container-components";
 
 function App() {
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <UserApp />
     </div>
   )
 }
