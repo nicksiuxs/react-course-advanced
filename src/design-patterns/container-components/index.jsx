14dcaf2feebcb99b6be8b33a3f794430d3257b8e
Nicolas Londono <nicksiuxs@gmail.com>
2025-06-23 20:57:47 -0500

    Add: DataSource component for data fetching and integrate it into the main app

--- src/design-patterns/container-components/index.jsx
+++ src/design-patterns/container-components/index.jsx
@@ -1,9 +1,16 @@
+import axios from "axios";
 import CurrentUserLoader from "./components/current-user-loader";
 import UserLoader from "./components/user-loader";
 import ResourceLoader from "./components/resource-loader";
+import DataSource from "./components/data-source";
 
 import { UserInfo } from "./components/user-info";
 import { BookInfo } from "./components/book-info";
+
+const getDataFromServer = async (url) => {
+  const response = await axios.get(url);
+  return response.data;
+}
 
 const App = () => {
   return (
@@ -28,12 +35,18 @@ const App = () => {
       </>
       <>
         {/* third container component (with props) */}
-        <ResourceLoader resourceUrl="/api/users/2" resourceName="user">
+        {/* <ResourceLoader resourceUrl="/api/users/2" resourceName="user">
           <UserInfo />
         </ResourceLoader>
         <ResourceLoader resourceUrl="/api/books/2" resourceName="book">
           <BookInfo />
-        </ResourceLoader>
+        </ResourceLoader> */}
+      </>
+      <>
+        {/* fourth container component (with props) */}
+        <DataSource getData={() => getDataFromServer("/api/users/2")} resourceName="user">
+          <UserInfo />
+        </DataSource>
       </>
     </>
   );
