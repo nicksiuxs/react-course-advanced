d4b407ecd7bd046228d9d50c70581728689fb2c9
Nicolas Londono <nicksiuxs@gmail.com>
2025-06-23 19:58:58 -0500

    Add: user loader component and update user retrieval in server

--- server.cjs
+++ server.cjs
@@ -13,18 +13,21 @@ let currentUser = {
 
 let users = [
   {
+    id: 1,
     name: "Sarah Waters",
     age: 55,
     country: "United Kingdom",
     books: ["Fingersmith", "The Night Watch"],
   },
   {
+    id: 2,
     name: "Haruki Murakami",
     age: 71,
     country: "Japan",
     books: ["Norwegian Wood", "Kafka on the Shore"],
   },
   {
+    id: 3,
     name: "Chimamanda Ngozi Adichie",
     age: 43,
     country: "Nigeria",
@@ -57,8 +60,8 @@ app.get("/current-user", (req, res) => res.json(currentUser));
 
 app.get("/users/:id", (req, res) => {
   const { id } = req.params;
-  console.log(id);
-  res.json(users.find((user) => user.id === id));
+  const user = users.find((user) => user.id === parseInt(id));
+  res.json(user);
 });
 
 app.get("/users", (req, res) => res.json(users));
--- src/design-patterns/container-components/index.jsx
+++ src/design-patterns/container-components/index.jsx
@@ -1,11 +1,30 @@
-import CurrentUserLoader  from "./components/current-user-loader";
+import CurrentUserLoader from "./components/current-user-loader";
+import UserLoader from "./components/user-loader";
+
 import { UserInfo } from "./components/user-info";
 
 const App = () => {
   return (
-    <CurrentUserLoader>
-      <UserInfo />
-    </CurrentUserLoader>
+    <>
+      <>
+      {/* first container component */}
+        <CurrentUserLoader>
+          <UserInfo />
+        </CurrentUserLoader>
+      </>
+      <>
+        {/* second container component (with props) */}
+        <UserLoader userId="1">
+          <UserInfo />
+        </UserLoader>
+        <UserLoader userId="2">
+          <UserInfo />
+        </UserLoader>
+        <UserLoader userId="3">
+          <UserInfo />
+        </UserLoader>
+      </>
+    </>
   );
 }
 
